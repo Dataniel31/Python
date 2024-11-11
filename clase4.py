066abadb68dce4bd74b99449a0ddcfb10deadecf
Dataniel31 <alexdspe02@gmail.com>
2024-11-10 21:29:59 -0500

    add clase4.py for string manipulation examples

---
+++ clase4.py
@@ -0,0 +1,6 @@
+name = '   Daniel Alex   '
+last_name = 'Suclle Luque'
+print(len(name)) # Longitud de la cadena
+print(name.lower()) # Convierte a minúsculas
+print(name.upper()) # Convierte a mayúsculas
+print(name.strip()) # Elimina los espacios en blanco al inicio y al final
